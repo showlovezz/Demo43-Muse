b3682c3b4535bc37b0d49b6b2e2659af2a5595fb
vita <dragic.hsieh@gmail.com>
2018-07-04 01:53:01 +0800

    Done add post controller actions and pages

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+	
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@posts = Post.order("created_at DESC")
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to @post, notice: "貼文已建立"
+		else
+			render "new", alert: "貼文建立失敗"
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update(post_params)
+			redirect_to @post, notice: "貼文已修改好"
+		else
+			render "edit", alert: "貼文修改失敗"
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to posts_path, alert: "貼文已刪除!"
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :link, :description, :rating)
+	end
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
+
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+	validates :title, length: {minimum: 5}
+	validates :link, presence: true
+	validates :description, presence: true
+	validates :rating, presence: true
 end
